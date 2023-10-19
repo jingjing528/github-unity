dace2a21e22efd40aeccca9080b377c1b6945e92
jingjing528 <xujingwords@163.com>
2023-10-20 00:05:42 +0800

    第二次修改

--- Assets/first.cs
+++ Assets/first.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 123 : MonoBehaviour
+public class first : MonoBehaviour
 {
+    string str = "first";
+    string str2 = "second";
     // Start is called before the first frame update
     void Start()
     {
